d50f561e0559858a7d5148b617bc70ce1ea0cc68
Alone799 <1499430394@qq.com>
2018-11-12 00:23:50 +0800

    商城day4的进度

--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,18 @@ var router = express.Router();
 //引入crypto模块
 var md5=require("crypto");
 
-//1 引入mysql模块
-var mysql= require('mysql');
-
-//2 链接数据库
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'smms'
-});
-
-//3 打开数据库链接
-connection.connect();
+//引入数据库链接模块
+var connection=require("./mysqlConn");
 
 /* 添加用户的路由 */
 router.post('/add', function(req, res, next) {
-  //2. 后端路由接收前端的数据
+  //后端路由接收前端的数据
   let {pass,username,region}=req.body;
 
   //对密码进行md5加密
   pass=md5.createHash("md5").update(pass).digest("hex");
 
-  //3. 链接数据库，把数据库写入数据库
+  //链接数据库，把数据库写入数据库
   //定义sql语句
   //let sqlStr=`insert into userTable(userName,userPwd,userGroup) values('${username}','${pass}','${region}')`;
   let sqlStr="insert into userTable(userName,userPwd,userGroup) values(?,?,?)"; //占位符
@@ -46,20 +35,20 @@ router.post('/add', function(req, res, next) {
 
 // 获取用户列表的路由
 router.get("/list",(req,res)=>{
-   //1. 构造sql语句
+   // 构造sql语句
    let sqlStr="select * from userTable order by u_id DESC";
 
-   //2. 执行sql语句
+   //执行sql语句
    connection.query(sqlStr,function (error,userlist) {
      if (error) throw error; //出错后面的代码不执行
-     //3. 返回查询的结果到前端（对象数组）
+     //返回查询的结果到前端（对象数组）
      res.send(userlist);
    });
 });
 
 // 删除用户的路由
 router.get("/del",(req,res)=>{
-   //3. 后端路由接收删除的id 返回结果到前端
+   //后端路由接收删除的id 返回结果到前端
    let id=req.query.id;
 
    //构造sql语句
@@ -72,7 +61,7 @@ router.get("/del",(req,res)=>{
      //"affectedRows":1, 返回受影响的行数，如果大于0就表示成功
      if(result.affectedRows>0){
        res.send({"isOk":true,"msg":"账号删除成功!"});
-       //res.redirect("member_list.html");  后端没有分离的做法【不推荐】
+       
      }
      else{
        res.send({"isOk":false,"msg":"账号删除失败!"});
@@ -83,24 +72,24 @@ router.get("/del",(req,res)=>{
 
 // 根据id获取用户数据的路由
 router.get("/getUserByID",(req,res)=>{
-  //1. 接收用户id
+  // 接收用户id
   let id=req.query.id;
   
-  //2. 构造sql语句
+  //构造sql语句
   let sqlStr="select * from userTable where u_id=?";
   let sqlParams=[id];
 
-  //3. 执行sql语句
+  //执行sql语句
   connection.query(sqlStr,sqlParams,function (error,userData) {
     if (error) throw error; //出错后面的代码不执行
-    //4. 返回查询的结果到前端（对象数组）
+    // 返回查询的结果到前端（对象数组）
     res.send(userData);
   });
 });
 
-//3）接收新的数据并把新的数据update到数据库中
+//接收新的数据并把新的数据update到数据库中
 router.post('/save', function(req, res, next) {
-  //2. 后端路由接收前端的数据
+  //后端路由接收前端的数据
   let {pass,username,region,u_id,oldPwd}=req.body;
   let newPass=pass; //只是为了名字更醒目
 
@@ -109,14 +98,14 @@ router.post('/save', function(req, res, next) {
     pass=md5.createHash("md5").update(newPass).digest("hex");
   }
 
-  //3. 链接数据库，把数据库写入数据库
+  //链接数据库，把数据库写入数据库
   //定义sql语句;
   let sqlStr="update userTable set userName=?,userPwd=?,userGroup=? where u_id=?"; //占位符
   let sqlParams=[username,pass,region,u_id]; //参数数组
   //执行sql语句
   connection.query(sqlStr,sqlParams, function (error, results) {
     if (error) throw error; //出错对象
-    //4. 返回处理的结果到前端
+    //返回处理的结果到前端
     //根据执行sql语句的结果返回json给前端
     //"affectedRows":1, 返回受影响的行数，如果大于0就表示成功
     if(results.affectedRows>0){
@@ -127,5 +116,58 @@ router.post('/save', function(req, res, next) {
     }
   });
 });
+
+//验证用户是否合法登录
+router.post("/loginCheck",(req,res,next)=>{
+  //后端路由接收前端传入的用户名和密码,并根据用户名和密码做数据库查询
+  let{username,checkPass} = req.body;
+  //准备sql语句
+  let sqlStr = "select u_id from userTable where userName=? and userPwd=?";
+  //对密码进行加密
+  checkPass = md5.createHash("md5").update(checkPass).digest("hex");
+  let sqlParams=[username,checkPass];
+
+  //后端查询到数据说明成功,否则失败,验证登录成功要写入cookie
+  connection.query(sqlStr,sqlParams,function(err,result){
+    //抛出错误
+    if(err) throw err;
+    //result成功的话是result的值,不成功是空数组
+    if(result.length>0){
+      //登录成功写入到cookie
+      res.cookie("username",username);
+      res.cookie("u_id",result[0].u_id);
+      res.send({isOk:true,msg:"用户登录成功!"});
+    }
+    else{
+      res.send({isOk:false,msg:"用户登录失败!"});
+    }
+
+  })
+});
+
+//退出登录就销毁cookie
+router.get("/signOut",(req,res)=>{
+  //清除cookie
+  res.clearCookie("username");
+  res.clearCookie("u_id");
+
+  //跳回登录页面
+  res.redirect("/signin.html");
+});
+
+
+//验证身份的合法性,有cookie合法,没有就不合法
+router.get("/checkState",(req,res)=>{
+  //读取cookie
+  var username = req.cookies.username;
+
+  //username不存在就是非法就跳转到登录页面
+  if(!username){
+    res.send("alert('非法入侵，请登录!');location.href='signin.html';")
+  }else{
+    res.send("");
+  }
+
+})
 
 module.exports = router;
